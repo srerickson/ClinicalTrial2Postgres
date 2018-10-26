a37a9ef372a026e26099210b574c20ca4dc2f30f
Seth Erickson <sr.erickson@gmail.com>
2018-10-26 10:30:18 -0400

    Update setup_postgres.sh

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir $(pwd)/postgres_db
-docker run -dit --rm --name ctPostgres -p 5432:5432 -v $(pwd)/postgres_db:/var/lib/postgresql/data \
+docker run -dit --name ctPostgres -p 5432:5432 -v $(pwd)/postgres_db:/var/lib/postgresql/data \
   -e POSTGRES_DB=ct -e POSTGRES_USER=ct postgres:11-alpine
